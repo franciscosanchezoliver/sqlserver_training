bce85f3e688453eda0797931d0003a36911a8dea
Francisco Sanchez Oliver <spuzi@hotmail.es>
2023-06-08 06:15:22 +0200

    Procedure that returns values
    Creation of differents procedures, ones
    that don't return values and other one
    that returns a value.
    
    Creation of a variable that store the
    result of a procedure

--- example_procedures.sql
+++ example_procedures.sql
@@ -22,3 +22,37 @@ EXEC pr_insertPerson 21, 'Sanchez Oliver', 'Francisco', 'Madrid', 'El Molar';
 
 -- Check that the new row has been inserted
 SELECT * FROM Persons where PersonID = 21;
+
+-- Let's create a procedure to search a person by its name
+CREATE PROCEDURE pr_searchPersonByName (@firstName VARCHAR(255))
+AS
+BEGIN
+
+	SELECT * 
+	FROM Persons
+	WHERE FirstName LIKE '%' + @firstName + '%'
+
+END
+GO
+
+-- Test the procedure by passing a name that we know that exists in the table
+EXEC pr_searchPersonByName 'Fran';
+
+-- Let's create a procedure that returns a value
+CREATE PROCEDURE pr_calculateAverage(@n1 DECIMAL(4,2), @n2 DECIMAL(4,2), @resu DECIMAL(4,2) OUTPUT)
+AS 
+BEGIN
+	SELECT @resu = (@n1 + @n2) / 2
+
+END
+GO
+
+-- Create a variable to store the result of the procedure that calculates the average
+DECLARE @resultVariable DECIMAL(4,2)
+
+-- Call the procedure that calculates the average and store the value in a variable
+EXECUTE pr_calculateAverage 5 , 6, @resultVariable OUTPUT
+
+-- Query the value of the variable
+SELECT @resultVariable as Average
+
